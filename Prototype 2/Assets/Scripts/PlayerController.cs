b122f52941079289eac6f0f2b1428ff175a46b32
peeryo <40718018+peeryo@users.noreply.github.com>
2021-11-16 09:08:58 -0800

    Why is it not committing

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizonalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
 
     // Start is called before the first frame update
@@ -17,12 +19,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizonalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizonalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         
     }
 }
